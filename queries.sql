56bf8bf1dcc0744ef73af4f4ae8c24efaca251cf
Christian Erick Contreras Cabrera <eri89--@hotmail.com>
2021-10-11 19:23:29 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -14,3 +14,15 @@ SELECT animals.name, animals.neutered FROM animals WHERE neutered = true;
 SELECT animals.name FROM animals WHERE name != 'Gabumon';
 SELECT animals.name, animals.weight_kg FROM animals 
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- 
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT ROUND(AVG(weight_kg),4) "Average weight" FROM animals;
+SELECT * FROM animals WHERE escape_attempts > 0 AND neutered = true;
+SELECT min(weight_kg), max(weight_kg) FROM animals;
+SELECT AVG(escape_attempts) "Escape attempts"
+FROM animals
+WHERE date_of_birth >= timestamp '1990-01-01 00:00:00'
+AND date_of_birth <= timestamp '2000-01-01 00:00:00';
